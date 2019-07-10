180122bb3b692d360a6b2f36ba2faa4505b6be15
Anuwat Songchumrong <anuwat_song@outlook.com>
2019-07-10 13:56:34 +0700

    Updates

---
+++ precache-manifest.597a2716943309e45290cee0bee3dfa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784018def8325f3553995ba3cf024265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fc519eeaf670e5ebcec",
+    "url": "/static/css/main.b969ee2c.chunk.css"
+  },
+  {
+    "revision": "fe7c49e1ea7221192cf2",
+    "url": "/static/js/2.dfb50a64.chunk.js"
+  },
+  {
+    "revision": "4fc519eeaf670e5ebcec",
+    "url": "/static/js/main.337d3acb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
